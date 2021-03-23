23c61b5e9cd10113a8aa95e78e5e5f275574df8c
Chowdhury-Anik <bloggeranik1986@gmail.com>
2021-03-23 21:32:36 +0600

    dynamic api and api parameter

--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const express = require('express');
 
 
 const app = express();
+
+const users = ['Manik', 'Bonik', 'Shalik', 'falink', 'Montu'];
+
 app.get('/', (req, res) => {
-    res.send('Thank you for calling me');
+
+    const products = {
+        desktop: 'intel',
+        price: 200,
+        laptop: 'AMD',
+        price: 300
+
+    }
+    res.send(products);
+});
+app.get('/products/desktop', (req, res) => {
+    res.send({ products: 'desktop', quantity: 10, price: 50000 });
+});
+
+app.get('/users/:id/', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({ id, name });
+
 })
 
 app.listen(3000, () => console.log('Listing to port 3000'));
